8da56104fb2e67fc36e84ed207bfa572d50fe4ce
Nicolai Syvertsen <saivert@saivert.com>
2015-12-31 05:00:56 +0100

    Fix autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
-aclocal --install || exit 1
+mkdir -p m4
+aclocal --install -I m4 || exit 1
 autoreconf --force --install -Wno-portability || exit 1
